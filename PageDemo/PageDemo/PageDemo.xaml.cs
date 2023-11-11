68e93241dd460b6e56e6f0d68c527f6667934564
Fedi Beltaief <fediblt3@gmail.com>
2023-11-11 11:42:38 +0100

    Initial commit

---
+++ PageDemo/PageDemo/PageDemo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PageDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageDemo : ContentPage
+    {
+        public PageDemo()
+        {
+            InitializeComponent();
+        }
+        public void PushPage(Object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PageDemo());
+        }
+        public void PopPage(Object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+        public void PushModalPage(Object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new PageDemo());
+        }
+        public void PopModalPage(Object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+
+    }
+}
